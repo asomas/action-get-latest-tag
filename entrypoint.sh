656aadf7e4a89a67c49936a39398a3aedaff9c52
feras <firas.almanna3@gmail.com>
2023-01-17 18:18:20 +0300

    use Perl regex

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,22 +11,22 @@ git fetch --prune --unshallow || true
 latest_tag=''
 
 if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
-  # Get a actual latest tag.
-  # If no tags found, supress an error. In such case stderr will be not stored in latest_tag variable so no additional logic is needed.
-  latest_tag=$(git describe --abbrev=0 --tags || true)
+    # Get a actual latest tag.
+    # If no tags found, supress an error. In such case stderr will be not stored in latest_tag variable so no additional logic is needed.
+    latest_tag=$(git describe --abbrev=0 --tags || true)
 else
-  # Get a latest tag in the shape of semver.
-  for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
-    tag="${ref#refs/tags/}"
-    if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
-      latest_tag="${tag}"
-      break
-    fi
-  done
+    # Get a latest tag in the shape of semver.
+    for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
+        tag="${ref#refs/tags/}"
+        if echo "${tag}" | grep -Pq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
+            latest_tag="${tag}"
+            break
+        fi
+    done
 fi
 
 if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
-  latest_tag="${INPUT_INITIAL_VERSION}"
+    latest_tag="${INPUT_INITIAL_VERSION}"
 fi
 
 echo "::set-output name=tag::${latest_tag}"
